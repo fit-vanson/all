1f4e0d62b236676e5751dfd573e040d503d8450b
Administrator <fit.vanson@gmail.com>
2021-11-30 13:28:49 +0700

    upload delete

---
+++ vendor/unisharp/laravel-filemanager/src/Controllers/DeleteController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace UniSharp\LaravelFilemanager\Controllers;
+
+use App\Models\FileManage;
+use Illuminate\Support\Facades\Storage;
+use UniSharp\LaravelFilemanager\Events\ImageIsDeleting;
+use UniSharp\LaravelFilemanager\Events\ImageWasDeleted;
+use function GuzzleHttp\Promise\all;
+
+class DeleteController extends LfmController
+{
+    /**
+     * Delete image and associated thumbnail.
+     *
+     * @return mixed
+     */
+    public function getDelete()
+    {
+        $item_names = request('items');
+        $id = request('id');
+        $errors = [];
+        foreach ($item_names as $name_to_delete) {
+            $file = $this->lfm->setName($name_to_delete);
+            if (!Storage::disk($this->helper->config('disk'))->exists($file->path('storage'))) {
+                return '<div style="color: red">Lỗi!</div>';
+            }
+
+            $file_to_delete = $this->lfm->pretty($name_to_delete);
+            $file_path = $file_to_delete->path();
+
+            event(new ImageIsDeleting($file_path));
+
+            if (is_null($name_to_delete)) {
+                array_push($errors, parent::error('folder-name'));
+                continue;
+            }
+
+            if (! $this->lfm->setName($name_to_delete)->exists()) {
+                array_push($errors, parent::error('folder-not-found', ['folder' => $file_path]));
+                continue;
+            }
+
+            if ($this->lfm->setName($name_to_delete)->isDirectory()) {
+                if (! $this->lfm->setName($name_to_delete)->directoryIsEmpty()) {
+                    FileManage::where('dir',request()->working_dir.'/'.$name_to_delete)->delete();
+//                    array_push($errors, parent::error('delete-folder'));
+//                    continue;
+                }
+            } else {
+                FileManage::whereIn('id',$id)->delete();
+                if ($file_to_delete->isImage()) {
+                    $this->lfm->setName($name_to_delete)->thumb()->delete();
+                }
+            }
+            $this->lfm->setName($name_to_delete)->delete();
+
+            event(new ImageWasDeleted($file_path));
+        }
+
+        if (count($errors) > 0) {
+            return $errors;
+        }
+
+        return parent::$success_response;
+    }
+}
