5c81a9a4a37f36d3654a77fe0bf9959324869266
Administrator <fit.vanson@gmail.com>
2021-11-30 13:08:43 +0700

    upload item

--- resources/views/content/file/index.blade.php
+++ resources/views/content/file/index.blade.php
@@ -7,68 +7,18 @@
 @endsection
 {{-- page styles --}}
 @section('page-style')
-{{--    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">--}}
+
 @endsection
 
 @section('content')
     <div class="card">
         <div class="card-body border-bottom">
-            <iframe src="/laravel-filemanager" style="width: 100%; height: 700px; overflow: hidden; border: none;"></iframe>
+            <iframe src="/laravel-filemanager" style="width: 100%; height:600px; overflow: hidden; border: none;"></iframe>
         </div>
     </div>
-
-
-
-
-
 @endsection
 
 @section('vendor-script')
-    <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
-
-    <script type="text/javascript">
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $('#uploadForm').on('submit',function (event){
-            console.log(event)
-            event.preventDefault();
-            var formData = new FormData($("#uploadForm")[0]);
-
-                $.ajax({
-                    // data: $('#projectForm2').serialize(),
-                    data: formData,
-                    url: "{{ route('file.upload') }}",
-                    type: "post",
-                    dataType: 'json',
-                    processData: false,
-                    contentType: false,
-                    success: function (data) {
-                        if(data.errors){
-                            for( var count=0 ; count <data.errors.length; count++){
-                                $("#uploadForm").notify(
-                                    data.errors[count],"error",
-                                    { position:"right" }
-                                );
-                            }
-                        }
-                        if(data.success){
-                            $.notify(data.success, "success");
-                            $('#uploadForm').trigger("reset");
-                            $('#ajaxModel').modal('hide');
-                            table.draw();
-                        }
-                    },
-                });
-
-
-        });
-
-
-    </script>
 @endsection
 
 
