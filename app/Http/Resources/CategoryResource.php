1fcb6c217ac0de11132691bdff57e99d85841edd
Administrator <fit.vanson@gmail.com>
2021-12-29 19:35:24 +0700

    API KEY

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,7 +18,7 @@ class CategoryResource extends JsonResource
             'id' => $this->id,
             'name' => $this->category_name,
             'view_count'=>$this->view_count,
-            'image' => asset('storage/'.$this->image),
+            'image' => asset('storage/categories/'.$this->image),
         ];
     }
 }
