84d3aa2ff7047f75534254223b704046ed3ac45c
Administrator <fit.vanson@gmail.com>
2021-12-29 17:29:59 +0700

    API

---
+++ app/Http/Resources/FeatureWallpaperResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FeatureWallpaperResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        foreach ($this->wallpaper->take(1) as $item){
+            return [
+                'categories' =>
+                    CategoryResource::collection($item->category),
+                'id' => $item->id,
+                'name' => $item->name,
+                'thumbnail_image' => asset('storage/wallpapers/thumbnail/'.$item->thumbnail_image),
+                'detail_image' => asset('storage/wallpapers/detail/' . $item->image),
+                'download_image' => asset('storage/wallpapers/download/' . $item->origin_image),
+                'like_count' => $item->like_count,
+                'views' => $item->view_count,
+                'feature' => $item->feature,
+                'created_at' => $item->created_at->format('d/m/Y'),
+            ];
+        }
+    }
+}
---
+++ app/Http/Resources/WallpaperResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WallpaperResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'categories' =>
+                CategoryResource::collection($this->category),
+            'id' => $this->id,
+            'name' => $this->name,
+            'thumbnail_image' => asset('storage/wallpapers/thumbnail/'.$this->thumbnail_image),
+            'detail_image' => asset('storage/wallpapers/detail/' . $this->image),
+            'download_image' => asset('storage/wallpapers/download/' . $this->origin_image),
+            'like_count' => $this->like_count,
+            'views' => $this->view_count,
+            'feature' => $this->feature,
+            'created_at' => $this->created_at->format('d/m/Y'),
+        ];
+    }
+}
