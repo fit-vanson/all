6e7ab514a29950ba256866830860006b61480288
Administrator <fit.vanson@gmail.com>
2022-05-10 15:24:20 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,8 @@ Route::group([
     Route::get('/get_category_details',[ApiController::class, 'get_category_details']);
     Route::get('/get_ads',[ApiController::class, 'get_ads']);
     Route::get('/get_settings',[ApiController::class, 'get_settings']);
+    Route::post('/update_view',[ApiController::class, 'update_view']);
+    Route::get('/update_download',[ApiController::class, 'update_download']);
 
     Route::get('/',[ApiController::class, 'index']);
     Route::get('/categories', [CategoryController::class, 'index']);
