fda2bfe5d0d9de6157f38e474b7f2cc6d3b41994
Administrator <fit.vanson@gmail.com>
2022-06-13 10:06:56 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -88,12 +88,13 @@ Route::group([
 
 
 Route::group([
-    "prefix" => "v3"
+    "prefix" => "v3",
 //    'middleware' => 'auth.apikey'
 ], function() {
     Route::get('version/check/{version}/{token_app}/{item_code}',[ApiV3Controller::class, 'checkCode']);
     Route::get('/',[ApiV3Controller::class, 'index']);
-    Route::get('first/{token_app}/{item_code}',[ApiV3Controller::class, 'first']);
-    Route::get('category/all/{token_app}/{item_code}',[ApiV3Controller::class, 'categoryAll']);
+    Route::get('first',[ApiV3Controller::class, 'first']);
+    Route::get('category/all',[ApiV3Controller::class, 'categoryAll']);
+    Route::get('wallpaper/all/{order}/{page}',[ApiV3Controller::class, 'wallpapersAll']);
 
 });
